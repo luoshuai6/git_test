fc4bc5a85321858592ac97c8d0ca95d5dc74a8ad
Luoshuai <13092978+luo-haoshuai@user.noreply.gitee.com>
2023-09-24 14:44:33 +0800

    master commit

--- src/main/java/com/powernode/git/GitTest.java
+++ src/main/java/com/powernode/git/GitTest.java
@@ -6,6 +6,5 @@ public class GitTest {
         System.out.println("hello git!2");
         System.out.println("hello git!3");
         System.out.println("hello git!4");
-        System.out.println("hot-fix change");
     }
 }
